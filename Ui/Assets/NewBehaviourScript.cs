ad0f2d14404343dd063e452f743b672559973f39
kachanov mykola <zirongames2@gmail.com>
2022-11-03 15:49:07 +0200

    1.UI Button

---
+++ Ui/Assets/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public void CLickButton()
+    {
+        Debug.Log("Clicked");
+    }
+    public void CLickButton2(string text)
+    {
+        Debug.Log(text);
+    }
+}
